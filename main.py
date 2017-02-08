98061642e649d8a9375aa48ec1f14296d79d8037
niklasphabian <griessbaum@gmail.com>
2017-02-08 10:37:33 -0800

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import pydap
+from flask import Flask
+from pydap.wsgi.app import DapServer
+
+app_debug = (os.environ.get('APP_DEBUG', 'False').lower() in ['true', 'yes', 'y', '1'])
+app_path = os.environ.get('APP_PATH', '/home/griessbaum/pydapApp/data/')
+app_host = os.environ.get('APP_HOST', '0.0.0.0')
+app_port = int(os.environ.get('APP_PORT', '8080'))
+
+app = Flask(__name__)
+app.wsgi_app = DapServer(app_path,'/home/griessbaum/pydapApp/templates/')
+
+if __name__ == "__main__":
+   app.run(host=app_host, debug=app_debug, port=app_port) 
---
+++ run.sh
@@ -0,0 +1,7 @@
+rsync -r --exclude=/handlers/ /home/griessbaum/pydap/src/pydap/ /home/griessbaum/pydapVirtualEnv/lib/python2.7/site-packages/pydap/
+
+python /home/griessbaum/pydapApp/main.py 
+
+
+#pip2 -q uninstall pydap.handlers.csv
+#pip2 install git+https://github.com/pydap/pydap.handlers.csv
