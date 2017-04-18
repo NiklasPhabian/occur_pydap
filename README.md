52fb375a9ca4250e5396b40a62f9c22e859d7e17
griessbaum <griessbaum@gmail.com>
2017-04-18 16:07:02 -0700

    added dockerfile

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # pydapApp
 This repository contains an exemplary pydap application and serves as an experimental server setup.
+
+build docker image:
+`docker build . -t occur_pydap`
+
+start docker instance:
+`docker run -p 8080:80 -v /data:/data occur_pydap`
+
+environment variables:
+  - APP_DEBUG (default: False)
+  - APP_PATH (default: /data)
+  - APP_HOST (default: 0.0.0.0)
+  - APP_PORT (default: 80)
--- app/main.py
+++ app/main.py
@@ -1,19 +1,14 @@
 import os
-import pydap
 from flask import Flask
 from pydap.wsgi.app import DapServer
 
 app_debug = (os.environ.get('APP_DEBUG', 'False').lower() in ['true', 'yes', 'y', '1'])
-app_path = os.environ.get('APP_PATH', 'data/')
-app_host = os.environ.get('APP_HOST', )
+app_path = os.environ.get('APP_PATH', '../data')
+app_host = os.environ.get('APP_HOST', '0.0.0.0')
 app_port = int(os.environ.get('APP_PORT', '8080'))
 
-app = Flask(__name__, static_url_path='')
-app.wsgi_app = DapServer('data/')
-
-@app.route('/static/<path:path>')
-def send_js(path):
-    return send_from_directory('static', path)
+app = Flask(__name__)
+app.wsgi_app = DapServer(app_path)
 
 if __name__ == "__main__":
    app.run(host=app_host, debug=app_debug, port=app_port) 
